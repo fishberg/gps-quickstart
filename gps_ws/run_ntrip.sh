54932544a067182dbb9935b2ae74e672f3b1bcbd
Andrew Fishberg <fishberg.dev@gmail.com>
2026-04-27 16:46:46 -0400

    changed host to ntrip.mit.edu

--- gps_ws/run_ntrip.sh
+++ gps_ws/run_ntrip.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-ros2 launch ntrip_client ntrip_client_launch.py host:="192.168.0.249" mountpoint:="u-blox" username:="test" password:="test"
+ros2 launch ntrip_client ntrip_client_launch.py host:="ntrip.mit.edu" mountpoint:="u-blox" username:="test" password:="test"
